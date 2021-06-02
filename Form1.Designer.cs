1ec535d3d98366b2e7a8e9edf65e9a4d0b1d06d5
thou-mayest <85231096+thou-mayest@users.noreply.github.com>
2021-06-02 17:38:50 +0100

    push

---
+++ Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace RemoteWindowsSide
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button2.Location = new System.Drawing.Point(412, 280);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 65);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Minimize Window";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(122, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(17, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(434, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "write this in options page on your phone";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(17, 124);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(245, 29);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "test";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(12, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "network SSID";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label4.Location = new System.Drawing.Point(12, 157);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(114, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Password";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(17, 185);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(245, 29);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.UseSystemPasswordChar = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::brahmiMouseMovDrag.Properties.Resources.logo_drag;
+            this.pictureBox1.Location = new System.Drawing.Point(310, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(216, 213);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.checkBox1.Location = new System.Drawing.Point(17, 220);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(195, 29);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Show password";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button3.Location = new System.Drawing.Point(17, 255);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(148, 58);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "create access point";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button4.Location = new System.Drawing.Point(173, 255);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(148, 58);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "close";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(538, 357);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DRAG MOUSE APP";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,455 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Net;
+using System.Net.Sockets;
+using WindowsInput.Native;
+using WindowsInput;
+using System.Diagnostics;
+using AudioSwitcher.AudioApi;
+using AudioSwitcher.AudioApi.CoreAudio;
+
+namespace RemoteWindowsSide
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, int dwExtraInfo);
+
+
+
+
+        public Dictionary<string, VirtualKeyCode> keycode = new Dictionary<string, VirtualKeyCode>();
+
+
+
+
+        private const int MOUSEEVENTF_MOVE = 0x0001; /* mouse move */
+        private const int MOUSEEVENTF_LEFTDOWN = 0x0002; /* left button down */
+        private const int MOUSEEVENTF_LEFTUP = 0x0004; /* left button up */
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x0008; /* right button down */
+        private const int MOUSEEVENTF_RIGHTUP = 0x0010;
+
+        [DllImport("user32.dll",
+            CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons,
+                    int dwExtraInfo);
+
+
+
+        [DllImport("User32")]
+        static extern bool SetCursorPos(int X, int Y); //user 32 move cursor
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+            keycode.Add("A", VirtualKeyCode.VK_A);
+            keycode.Add("Z", VirtualKeyCode.VK_Z);
+            keycode.Add("E", VirtualKeyCode.VK_E);
+            keycode.Add("R", VirtualKeyCode.VK_R);
+            keycode.Add("T", VirtualKeyCode.VK_T);
+            keycode.Add("Y", VirtualKeyCode.VK_Y);
+            keycode.Add("U", VirtualKeyCode.VK_U);
+            keycode.Add("I", VirtualKeyCode.VK_I);
+            keycode.Add("O", VirtualKeyCode.VK_O);
+            keycode.Add("P", VirtualKeyCode.VK_P);
+            keycode.Add("Q", VirtualKeyCode.VK_Q);
+            keycode.Add("S", VirtualKeyCode.VK_S);
+            keycode.Add("D", VirtualKeyCode.VK_D);
+            keycode.Add("F", VirtualKeyCode.VK_F);
+            keycode.Add("G", VirtualKeyCode.VK_G);
+            keycode.Add("H", VirtualKeyCode.VK_H);
+            keycode.Add("J", VirtualKeyCode.VK_J);
+            keycode.Add("K", VirtualKeyCode.VK_K);
+            keycode.Add("L", VirtualKeyCode.VK_L);
+            keycode.Add("M", VirtualKeyCode.VK_M);
+            keycode.Add("W", VirtualKeyCode.VK_W);
+            keycode.Add("X", VirtualKeyCode.VK_X);
+            keycode.Add("C", VirtualKeyCode.VK_C);
+            keycode.Add("V", VirtualKeyCode.VK_V);
+            keycode.Add("B", VirtualKeyCode.VK_B);
+            keycode.Add("N", VirtualKeyCode.VK_N);
+
+            keycode.Add(" ", VirtualKeyCode.SPACE);
+
+            keycode.Add("0", VirtualKeyCode.NUMPAD0);
+            keycode.Add("1", VirtualKeyCode.NUMPAD1);
+            keycode.Add("2", VirtualKeyCode.NUMPAD2);
+            keycode.Add("3", VirtualKeyCode.NUMPAD3);
+            keycode.Add("4", VirtualKeyCode.NUMPAD4);
+            keycode.Add("5", VirtualKeyCode.NUMPAD5);
+            keycode.Add("6", VirtualKeyCode.NUMPAD6);
+            keycode.Add("7", VirtualKeyCode.NUMPAD7);
+            keycode.Add("8", VirtualKeyCode.NUMPAD8);
+            keycode.Add("9", VirtualKeyCode.NUMPAD9);
+
+
+            //int vol = 10;
+            ////AudioSwitcher.AudioApi.CoreAudio.CoreAudioDevice audioDevice = new AudioSwitcher.AudioApi.CoreAudio.CoreAudioController().DefaultPlaybackDevice;
+
+            ////audioDevice.Volume = vol;
+
+
+
+            //CoreAudioDevice defaultPlaybackDevice = new CoreAudioController().DefaultPlaybackDevice;
+
+            //defaultPlaybackDevice.Volume = vol;
+
+
+            
+
+
+
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+            label1.Text = GetLocalIPAddress();
+
+            Task main = new Task(WaitForConn);
+            main.Start();
+            //await WaitForConn();
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            for (int x = 0; x < 4; x++)
+                SetCursorPos(Cursor.Position.X + 10, System.Windows.Forms.Cursor.Position.Y + 10);
+
+        }
+
+
+        ASCIIEncoding encoder = new ASCIIEncoding();
+        TcpListener server = new TcpListener(IPAddress.Any, 1997);
+
+
+
+        public void WaitForConn()
+        {
+
+            server.Start();
+
+            accept_conn();
+
+        }
+
+        public void  accept_conn()
+        {
+            server.BeginAcceptTcpClient(handle_conn, server);
+
+            //creates the TcpClient
+
+
+
+            //await accept_conn(client);
+
+        }
+
+        public string tempMsg = "";
+        public string message = "";
+        private void handle_conn(IAsyncResult result)
+        {
+
+
+            
+            TcpClient client = server.EndAcceptTcpClient(result);
+
+            NetworkStream ns = client.GetStream();
+
+            byte[] msg = new byte[1024];     //the messages arrive as byte array
+            ns.Read(msg, 0, msg.Length);
+
+
+            accept_conn();
+
+            //MessageBox.Show(encoder.GetString(msg));
+
+            message = encoder.GetString(msg);
+
+            //once again, checking for any other incoming connections
+
+            // NetworkStream ns = client.GetStream();
+            //while(ns.DataAvailable)
+            // {
+            //     byte[] msg = new byte[1024];     //the messages arrive as byte array
+            //     ns.Read(msg, 0, msg.Length);
+
+            // }
+
+            //MessageBox.Show(encoder.GetString(msg));
+
+            //message = encoder.GetString(msg);
+
+
+
+
+            if (message.Contains("mmov_"))
+            {
+                tempMsg = message.Substring(5, message.Length - 5);
+                movMous(tempMsg);
+            }
+
+            if (message.Contains("click"))
+            {
+
+                LeftClick(Cursor.Position.X, Cursor.Position.Y);
+            }
+
+            if (message.Contains("Rc_"))
+            {
+
+                RightClick(Cursor.Position.X, Cursor.Position.Y);
+            }
+
+            if (message.Contains("drag_"))
+            {
+
+                MouseDrag(Cursor.Position.X, Cursor.Position.Y);
+            }
+
+            if (message.Contains("ENTER_sim"))
+            {
+
+                enterSim();
+            }
+
+            if (message.Contains("Butt_"))
+            {
+                SendKey(message.Substring(5, message.Length - 5));
+            }
+
+            if(message.Contains("volum_"))
+            {
+
+                controleAudio(message);
+            }
+
+        }
+
+        
+        public void controleAudio(string msg)
+        {
+
+            if(msg.Substring(6, msg.Length - 6).Contains("up"))
+            {
+                keybd_event((byte)Keys.VolumeUp, 0, 0, 0);
+            }
+            if(msg.Substring(6, msg.Length - 6).Contains("down"))
+            {
+                keybd_event((byte)Keys.VolumeDown, 0, 0, 0);
+            }
+            if(msg.Substring(6, msg.Length - 6).Contains("mute"))
+            {
+                keybd_event((byte)Keys.VolumeMute, 0, 0, 0);
+            }
+
+
+        }
+
+
+        public void enterSim()
+        {
+            sim.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+        }
+
+        public void LeftClick(int x, int y)
+        {
+            SetCursorPos(x, y);
+
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
+
+
+            //mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
+        }
+
+        public void RightClick(int x , int y)
+        {
+            SetCursorPos(x, y);
+            mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, 0);
+
+            mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, 0);
+
+
+        }
+
+        public void MouseDrag(int x , int y)
+        {
+
+            SetCursorPos(x, y);
+
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
+        }
+
+       
+
+        double Rx,Ry;
+        public void movMous(string val)
+        {
+            //for (int i = 0; i < 10 ; i++)
+            //{
+            //    SetCursorPos(Cursor.Position.X + 10,Cursor.Position.Y );
+            //    await Task.Delay(1);
+            //}
+
+            int x, y;
+            string[] values = val.Split('_');
+
+            double.TryParse(values[0],out Rx);
+            double.TryParse(values[1], out Ry);
+
+
+
+            x =(int) Math.Round(Rx);
+            y = (int)Math.Round(Ry);
+
+
+            SetCursorPos(Cursor.Position.X + x , Cursor.Position.Y + y ) ;
+        }
+
+       
+
+
+       
+        InputSimulator sim = new InputSimulator();
+        public void SendKey(string msg)
+        {
+            if (msg.Contains("__backspace"))
+            {
+                sim.Keyboard.KeyPress(VirtualKeyCode.BACK);
+                
+            }
+            else
+            {
+
+                sim.Keyboard.TextEntry(msg);
+
+
+              
+            }
+               
+           
+        }
+
+
+        public void sendTcp()
+        {
+            IPEndPoint ip = new IPEndPoint(IPAddress.Loopback, 1997);
+
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+           
+            if (!server.Connected)
+            {
+                try
+                {
+                    server.Connect(ip);
+                }
+                catch (Exception x)
+                {
+
+                    MessageBox.Show(x.Message);
+                }
+            }
+               
+
+
+                server.Send(Encoding.ASCII.GetBytes("TEST BYTES"));
+
+        }
+
+       
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+
+            //var host2 = Dns.GetHostAddresses(Dns.GetHostName());
+
+            //foreach (var item in host2)
+            //{
+            //    MessageBox.Show(item.MapToIPv4().ToString());
+            //    MessageBox.Show(item.AddressFamily.ToString());
+            //}
+
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork) 
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Button3_Click_1(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Contains(" "))
+            {
+
+                textBox1.Text = textBox1.Text.Replace(' ', '_');
+            }
+            if (textBox1.Text != "" & textBox2.Text.Length > 8)
+            {
+                startNetwork();
+            }
+            else
+            {
+                MessageBox.Show("SSID should be more than 8 characters without spaces or special characters, wifi should be open", "3ASBA YZZI BLA TA8FIS", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Stop);
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            //sendTcp();
+             this.WindowState =  FormWindowState.Minimized;
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void startNetwork()
+        {
+            Process SetLan = new Process();
+            SetLan.StartInfo.FileName = "netsh.exe";
+            SetLan.StartInfo.Arguments = "wlan set hostednetwork mode=allow ssid=" + textBox1.Text + " key=" + textBox2.Text;
+            SetLan.StartInfo.UseShellExecute = false;
+            SetLan.StartInfo.RedirectStandardOutput = true;
+            SetLan.StartInfo.CreateNoWindow = true;
+
+            SetLan.Start();
+            //string output = SetLan.StandardOutput.ReadToEnd();
+            //MessageBox.Show(output);
+
+            Process LaunchLan = new Process();
+            LaunchLan.StartInfo.FileName = "netsh.exe";
+            LaunchLan.StartInfo.Arguments = "wlan start hostednetwork";
+            LaunchLan.StartInfo.UseShellExecute = false;
+            LaunchLan.StartInfo.RedirectStandardOutput = true;
+            LaunchLan.StartInfo.CreateNoWindow = true;
+
+            LaunchLan.Start();
+
+        }
+    }
+}
